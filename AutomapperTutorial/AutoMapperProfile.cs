d059df10d19f71d010be4f78903611cbb61cb17c
yslavko <yslavko@gmail.com>
2023-05-11 22:52:00 +0200

    Add project files.

---
+++ AutomapperTutorial/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace AutomapperTutorial
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            //CreateMap<SuperHero, SuperHeroDto>();
+            //CreateMap<SuperHeroDto, SuperHero>();
+            CreateMap<SuperHero, SuperHeroDto>().ReverseMap();
+        }
+    }
+}
